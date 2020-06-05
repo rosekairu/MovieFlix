f7dafe8871a23bad13ced6fac4185074de83f27f
Rose Kairu <rosekairu@gmail.com>
2020-06-05 17:53:33 +0300

    git ignore

--- start.sh
+++
@@ -1,7 +0,0 @@
-export MOVIE_API_KEY='91094c0bd8412061326498e68da939bd'
-export SECRET_KEY='XPXXYJYX'
-
-export MAIL_USERNAME=moringawatchlist@gmail.com
-export MAIL_PASSWORD=XPXXYJYX
-
-python3.6 manage.py server
